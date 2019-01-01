8646d69037b3c9a8fff4bf76534ef85f7321ee07
marco <marco.magnatti@gmail.com>
2019-01-01 15:08:46 +0100

    routeGuard service forces login to access reserved pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,18 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 // routes are defined adding objects to this array
 const routes: Routes = [
   {path: '', component: LoginComponent},                // default path goes to login
   {path: 'login', component: LoginComponent},
-  {path: 'welcome/:name', component: WelcomeComponent}, // with this, i'm saying that i expect a parameter "name" in the query string/url
-  {path: 'todos', component: ListTodosComponent},
-  {path: 'logout', component: LogoutComponent},
+  // with this, i'm saying that i expect a parameter "name" in the query string/url
+  // the second arg is the component that's going to be initialized when the route is activated
+  // the third is the CanActivate interface: you implement this interface in a Service to define boolean activation logic
+  {path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService]},
+  {path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService]},
+  {path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService]},
 
   {path: '**', component: ErrorComponent}               // everything else goes here
 
